9018bf8e47a0e2d95417dd2826aaa7a275ae0433
Anish-Guduri <71063609+Anish-Guduri@users.noreply.github.com>
2025-03-16 00:16:16 +0530

    Update server.js
    port changed to 8080

--- product-catalogue-backend/server.js
+++ product-catalogue-backend/server.js
@@ -177,7 +177,7 @@ app.post('/api/products', upload.single('image'), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
